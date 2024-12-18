f0aec4ebf1261f45b74cc238037dd41b9dd246f0
StarFallenJax <jaxplarsen@gmail.com>
2024-12-18 12:45:26 -0800

    Barebones file

---
+++ src/Frame.java
@@ -0,0 +1,19 @@
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.GridLayout;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.IOException;
+
+public class Frame {
+}
