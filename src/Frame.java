e616da6e95798085ecab8add1a8edbb3e8595b02
StarFallenJax <jaxplarsen@gmail.com>
2024-12-18 13:24:16 -0800

    Added skeleton of frame

--- src/Frame.java
+++ src/Frame.java
@@ -7,13 +7,95 @@ import java.awt.GridLayout;
 import java.awt.Point;
 import java.awt.Rectangle;
 import java.awt.Toolkit;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
+import java.awt.event.*;
 import java.io.IOException;
 
-public class Frame {
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.Timer;
+
+import java.util.Random;
+import java.util.*;
+
+public class Frame extends JPanel implements ActionListener, MouseListener, KeyListener {
+
+    public void paint(Graphics g) {
+        super.paintComponent(g);
+    }
+
+
+    public static void main(String[] args) {
+        Frame f = new Frame();
+
+    }
+
+    public Frame() {
+
+        JFrame f = new JFrame("j nation");
+        f.setSize(new Dimension(800, 600));
+        f.setBackground(Color.white);
+        f.add(this);
+        f.setResizable(false);
+        f.addMouseListener(this);
+        f.addKeyListener(this);
+
+        setCursor(Toolkit.getDefaultToolkit().createCustomCursor(
+                new ImageIcon("lol.png").getImage(),
+                new Point(0, 0), "custom cursor"));
+
+        Timer t = new Timer(16, this);
+        t.start();
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setVisible(true);
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent arg0) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent arg0) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent arg0) {
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent arg0) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent arg0) {
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+        repaint();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent arg0) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent arg0) {
+
+    }
+
+    @Override
+    public void keyTyped(KeyEvent arg0) {
+
+    }
+
 }
